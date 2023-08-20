eee7cc40223c16e4fb4ca6183f686030ad2c79ad
Gleison Araujo <gleison@touchtec.com.br>
2023-08-20 13:38:54 -0300

    add a keymap for navigate between buffers

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,6 @@
 -- Simple way to go back to netrw
 vim.keymap.set('n', '<leader>q', vim.cmd.Ex)
+-- Navigate between buffers
+vim.keymap.set('n', '<leader>n', vim.cmd.bn)
+vim.keymap.set('n', '<leader>p', vim.cmd.bp)
+
