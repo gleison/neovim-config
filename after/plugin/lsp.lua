e96393ba89cf48e3d4a395af0f2512edcf1315c1
Gleison Araujo <gleison@touchtec.com.br>
2023-08-30 18:13:56 -0300

    change location of diagnostic float config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,8 +3,31 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
+lsp.on_attach(function(_, bufnr) --client
+    lsp.default_keymaps({
+        buffer = bufnr,
+        preserve_mappings = false
+    })
+    vim.api.nvim_create_autocmd("CursorHold", {
+        buffer = bufnr,
+        callback = function()
+            local opts = {
+                focusable = false,
+                close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+                border = 'rounded',
+                source = 'always',
+                prefix = ' ',
+                scope = 'cursor',
+            }
+            vim.diagnostic.open_float(nil, opts)
+        end
+    })
+end)
+
 local root_markers = {'gradlew', 'mvnw', '.git'}
-local root_dir = vim.fs.dirname(vim.fs.find(root_markers, { upward = true })[1]),
+local root_dir = function ()
+    return vim.fs.dirname(vim.fs.find(root_markers, { upward = true })[1])
+end
 
 lsp.configure('jdtls', {
   cmd_env = {
