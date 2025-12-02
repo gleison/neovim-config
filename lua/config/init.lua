618710bc2d738a34d927dc7d1a4799fcb054bc30
Gleison <gleisonsta@gmail.com>
2025-12-02 11:15:33 -0300

    configure nvim-lspconfig and lua_ls. Need to manually install lua-language-server

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -3,6 +3,17 @@ require('config.remap')
 require('config.set')
 
 vim.pack.add({
-    { src = 'https://github.com/rose-pine/neovim', name = 'rose-pine' }
+    { src = 'https://github.com/rose-pine/neovim', name = 'rose-pine' },
+    { src = 'https://github.com/neovim/nvim-lspconfig', name = 'lspconfig'}
 })
 vim.cmd('colorscheme rose-pine')
+
+vim.lsp.config('lua_ls', {
+    settings = {
+    Lua = {
+      diagnostics = { globals = {'vim'} },
+    },
+  },
+})
+vim.lsp.enable('lua_ls')
+
