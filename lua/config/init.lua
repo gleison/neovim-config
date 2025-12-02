cb28f4b530e6bfaf3397b2e11494d27b4e80eb79
Gleison <gleisonsta@gmail.com>
2025-12-02 11:00:13 -0300

    Configure rose-pine colorscheme

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,8 @@
 -- Initial configurations
 require('config.remap')
 require('config.set')
+
+vim.pack.add({
+    { src = 'https://github.com/rose-pine/neovim', name = 'rose-pine' }
+})
+vim.cmd('colorscheme rose-pine')
