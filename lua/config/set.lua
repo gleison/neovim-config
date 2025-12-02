4b7852b8c949abf605c6eb9a84e7433c3721aceb
Gleison <gleisonsta@gmail.com>
2025-12-02 10:37:20 -0300

    Initial Configuration: vim options and file explorer keymap

---
+++ lua/config/set.lua
@@ -0,0 +1,33 @@
+-- Line Number
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Identation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- No wrap lines
+vim.opt.wrap = false
+
+-- No backup files
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Incremental search
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Some colors
+vim.opt.termguicolors = true
+
+-- Scroll always show last 8 lines
+vim.opt.scrolloff = 8
+
+-- Faster completion
+vim.opt.updatetime = 50
+
+-- Show 120 colum limit
+vim.opt.colorcolumn = "120"
