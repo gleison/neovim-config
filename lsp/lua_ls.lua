e65a4b7f9efe08f86e7bada978e1a46163cb61ce
Gleison <gleisonsta@gmail.com>
2025-12-03 21:47:16 -0300

    Configure lspconfig, mason and mason-lspconfig. Also configure lua language server

---
+++ lsp/lua_ls.lua
@@ -0,0 +1,13 @@
+return {
+    settings = {
+        Lua = {
+            diagnostics = { globals = {'vim'} },
+            runtime = { version = 'LuaJIT' },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file('', true),
+                checkThirdParty = false,
+            },
+            telemetry = { enable = false }
+        },
+    }
+}
