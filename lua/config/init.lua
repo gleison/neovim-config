6f8a09b12b820d3232ee7f508828316eaf735b41
Gleison <gleisonsta@gmail.com>
2025-12-03 21:47:16 -0300

    Configure lspconfig, mason and mason-lspconfig. Also configure lua language server

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -4,16 +4,23 @@ require('config.set')
 
 vim.pack.add({
     { src = 'https://github.com/rose-pine/neovim', name = 'rose-pine' },
-    { src = 'https://github.com/neovim/nvim-lspconfig', name = 'lspconfig'}
+    { src = 'https://github.com/neovim/nvim-lspconfig', name = 'lspconfig'},
+    { src = 'https://github.com/mason-org/mason.nvim', name = 'mason'},
+    { src = 'https://github.com/mason-org/mason-lspconfig.nvim', name = 'mason-lspconfig'}
 })
+
 vim.cmd('colorscheme rose-pine')
 
-vim.lsp.config('lua_ls', {
-    settings = {
-    Lua = {
-      diagnostics = { globals = {'vim'} },
-    },
-  },
-})
 vim.lsp.enable('lua_ls')
 
+require('mason').setup({
+    ui = { icons = {
+        package_installed = '✓',
+        package_pending = '➜',
+        package_uninstalled = '✗'
+    } }
+})
+
+require('mason-lspconfig').setup({
+    ensure_installed = { 'lua_ls' }
+})
