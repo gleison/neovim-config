c1c903119ec2f859fd266d390ac7ad0666b5a638
Gleison <gleisonsta@gmail.com>
2026-06-03 17:52:34 -0300

    nvim-java without spring-boot

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -6,12 +6,22 @@ vim.pack.add({
     { src = 'https://github.com/rose-pine/neovim', name = 'rose-pine' },
     { src = 'https://github.com/neovim/nvim-lspconfig', name = 'lspconfig'},
     { src = 'https://github.com/mason-org/mason.nvim', name = 'mason'},
-    { src = 'https://github.com/mason-org/mason-lspconfig.nvim', name = 'mason-lspconfig'}
+    { src = 'https://github.com/mason-org/mason-lspconfig.nvim', name = 'mason-lspconfig'},
+    { src = 'https://github.com/MunifTanjim/nui.nvim', name='nui'},
+    { src = 'https://github.com/mfussenegger/nvim-dap', name='nvim-dap'},
+    { src = 'https://github.com/nvim-java/nvim-java', name='nvim-java'}
 })
 
 vim.cmd('colorscheme rose-pine')
 
 vim.lsp.enable('lua_ls')
+vim.lsp.enable('jdtls')
+
+require('java').setup({
+    spring_boot_tools = {
+        enable = false
+    }
+})
 
 require('mason').setup({
     ui = { icons = {
