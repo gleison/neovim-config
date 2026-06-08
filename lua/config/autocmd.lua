d155661756031d3482d4bae9e6f45c5e3def547e
Gleison <gleisonsta@gmail.com>
2026-06-08 16:21:56 -0300

    configurando o telescope, ui-select do telescope e ui-input com dress. Adicionando alguns remapeamentos usando o telescope

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -5,3 +5,25 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.hl.on_yank({ higroup = 'IncSearch', timeout = 200 })
     end,
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('telescope-lsp-attach', { clear = true }),
+    callback = function(event)
+        local buf = event.buf
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', 'gr', builtin.lsp_references, { buffer = buf, desc = '[G]oto [R]eferences' })
+        vim.keymap.set('n', 'gi', builtin.lsp_implementations, { buffer = buf, desc = '[G]oto [I]mplementation' })
+        vim.keymap.set('n', 'gd', builtin.lsp_definitions, { buffer = buf, desc = '[G]oto [D]efinition' })
+        vim.keymap.set('n', 'gt', builtin.lsp_type_definitions, { buffer = buf, desc = '[G]oto [T]ype Definition' })
+
+        vim.keymap.set('n', 'gO', builtin.lsp_document_symbols, { buffer = buf, desc = 'Open Document Symbols' })
+
+        vim.keymap.set('n', 'gW', builtin.lsp_dynamic_workspace_symbols, { buffer = buf, desc = 'Open Workspace Symbols' })
+
+        vim.keymap.set('n', 'rn', vim.lsp.buf.rename, { buffer = buf, desc ='[R]e[n]ame' })
+        vim.keymap.set({ 'n', 'v' }, 'fs', vim.lsp.buf.format, { buffer = buf, desc = 'Format Code' })
+
+        vim.keymap.set( { 'n', 'v' }, 'ga', vim.lsp.buf.code_action, { buffer = buf, desc ='[G]oto Code [A]ction' })
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = buf, desc ='[G]oto [D]eclaration' })
+    end,
+})
---
+++ lua/plugins/search_ui.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        'nvim-telescope/telescope-ui-select.nvim',
+        dependencies = {
+            {
+                'nvim-telescope/telescope.nvim',
+                dependencies = {
+                    'nvim-lua/plenary.nvim',
+                },
+                keys = {
+                    { '<leader>ff', '<cmd>Telescope find_files<cr>' },
+                    { '<leader>fb', '<cmd>Telescope buffers<cr>' },
+                    { '<leader>fg', '<cmd>Telescope live_grep<cr>' },
+                    { '<leader>fh', '<cmd>Telescope help_tags<cr>' },
+                }
+            }
+        },
+        config = function()
+            require('telescope').setup ({
+                extensions = {
+                    ['ui-select'] = require('telescope.themes').get_dropdown({})
+                }
+            })
+            require('telescope').load_extension('ui-select')
+        end,
+    },
+    {
+        'Axlefublr/dress.nvim',
+        config = function()
+            require('dress').setup({
+                win_opts = {
+                    border = 'single',
+                }
+            })
+        end,
+    },
+}
