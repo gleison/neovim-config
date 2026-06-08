b990fec2629bdfd33049e22743a705a287e09afc
Gleison <gleisonsta@gmail.com>
2026-06-08 13:03:59 -0300

    Configurando remapeamentos para acessar o netrw, navegação entre buffers e fechar um buffer

---
+++ lua/config/remap.lua
@@ -0,0 +1,10 @@
+-- Access Netrw
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+
+-- Navigate between buffers
+vim.keymap.set('n', '<leader>n', vim.cmd.bn)
+vim.keymap.set('n', '<leader>p', vim.cmd.bp)
+
+-- Close a buffer
+vim.keymap.set('n', '<leader>q', vim.cmd.bd)
+
