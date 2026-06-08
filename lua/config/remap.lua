0065a935a32b960771e1a8404f21cf3b7e36f909
Gleison <gleisonsta@gmail.com>
2026-06-08 17:23:09 -0300

    Adicionado remapeamento para mover linhas para cima e para baixo

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -8,5 +8,8 @@ vim.keymap.set('n', '<leader>p', vim.cmd.bp)
 -- Close a buffer
 vim.keymap.set('n', '<leader>q', vim.cmd.bd)
 
+vim.keymap.set({ "n", "v" }, "<A-Up>", ":m .-2<CR>==", { desc = "Move Line Up" })
+vim.keymap.set({ "n", "v" }, "<A-Down>", ":m .+1<CR>==", { desc = "Move Line Down" })
+
 -- Clear highlight search with ESC
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
