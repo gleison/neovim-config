4e4004e1c6f02e9a1ee3d61fb2821aee2ec7ddcf
Gleison <gleisonsta@gmail.com>
2026-06-08 13:47:34 -0300

    configurando o gerenciador de plugins lazy e o highlight para o yank

---
+++ lua/config/autocmd.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = 'YankHighlight',
+    callback = function()
+        vim.hl.on_yank({ higroup = 'IncSearch', timeout = 200 })
+    end,
+})
--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,2 +1,20 @@
 require('config.remap')
 require('config.set')
+require('config.autocmd')
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        '--branch=stable',
+        'https://github.com/folke/lazy.nvim.git',
+        lazypath,
+    }
+end
+vim.opt.rtp:prepend(lazypath)
+require('lazy').setup({
+    spec = { import = 'plugins'},
+    rocks = { enabled = false }
+})
